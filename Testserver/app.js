99a0cec1ef75363d0a080a32f2e5200eab2cf83e
Cheon_Main <orangelite@naver.com>
2015-05-26 17:06:31 +0900

    first

---
+++ Testserver/app.js
@@ -0,0 +1,85 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+var fs = require('fs');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/index', function(req,res){
+	console.log("indexpage");
+	fs.readFile('public/index.html',function(error,data){
+		if(error){
+			console.log(error);
+		}
+		else{
+			res.writeHead(200,{'Content-Type':'text/html'});
+			res.end(data);
+		}
+	});
+});
+
+var httpServer = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+/*/upgrade http server to socket.io server
+var io = require('socket.io').listen(httpServer);
+ 
+io.sockets.on('connection',function(socket){
+   socket.emit('toclient',{msg:'Welcome !'});
+   socket.on('test',function(data){
+	   console.log('Message from client :'+data.msg);
+	   
+   });
+   
+   socket.on('fromclient',function(data){
+       socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+       socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
+       console.log('Message from client :'+data.msg);
+   });
+});
+*/
+var io = require('socket.io').listen(httpServer);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+    
+    socket.on('login', function (data) {
+        console.log(data);
+        socket.emit('loginok',data);
+    });
+    
+    socket.on('logout', function () {
+        console.log("logout");
+    });
+    
+});
---
+++ Testserver/public/js/momo.js
@@ -0,0 +1,36 @@
+/**
+ * New node file
+ */
+var strst = '<section id="portfolio" class="portfolio">';
+	strst += '<div class="container">';
+	strst += '       <div class="row">';
+	strst += '           <div class="col-lg-10 col-lg-offset-1 text-center">';
+	strst += '               <h2>Our Work</h2>';
+	strst += '               <hr class="small">';
+	strst += '              <div class="row">';
+                    
+      
+var strmd = '';
+
+for(var i=1;i<=4;i++){
+	var tempstr = ' <div class="col-md-6">';
+		tempstr +=	'<div class="portfolio-item">';
+		tempstr +=	'  <a href="#">';
+		tempstr +=	'       <img class="img-portfolio img-responsive" src="img/portfolio-'+i+'.jpg">';
+		tempstr +=	'    </a>';
+		tempstr +=	'	</div>';
+		tempstr +=	'	</div>';
+		
+	strmd+=tempstr;
+}
+	
+                        
+var stred = ' </div>';
+	stred+='<a href="#" class="btn btn-dark">View More Items</a>';
+	stred+='            </div>';
+	stred+='        </div>';
+	stred+='     </div>;';
+	stred+='</section>;';
+	
+
+var optstr = strst + strmd + stred;
