fcd62e49f0dd0b1746fbf05eb9bd6c16858fb90c
Cheon_Main <orangelite@naver.com>
2015-05-31 00:59:34 +0900

    login & signup 기본처리

--- Testserver/app.js
+++ Testserver/app.js
@@ -11,8 +11,9 @@ var express = require('express')
 
 var app = express();
 var fs = require('fs');
+var md5 = require('MD5');
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var sqlconnection = mysql.createConnection({
 	host : 'mydb.cwb75deezrvy.ap-northeast-1.rds.amazonaws.com',
 	user : 'cheon',
 	password : '1c235887',
@@ -74,17 +75,65 @@ io.sockets.on('connection',function(socket){
 */
 var io = require('socket.io').listen(httpServer);
 
+
 io.sockets.on('connection', function (socket) {
  
     
     socket.on('signup',function(data){
-    	console.log(data);
-    	socket.emit(data.name);
+    	
+    	
+    	var dt = new Date(data.birth);
+    	var ndt = new Date();
+    	var age = ndt.getFullYear() - dt.getFullYear() + 1;
+    	
+    	var gender = "";
+    	if(data.gender == 'male')
+    		gender = 1;
+    	else
+    		gender = 2;
+    	
+    	
+    	var queryset = {
+    			Uname : data.name,
+    			Uemail : data.email,
+    			Upassword : md5(data.passward),
+    			Ugender : gender,
+    			Ubirthday : dt,
+    			Uage : age,
+    			Uinterest : "ss"
+    	}
+    	
+    	sqlconnection.query('insert into myDB.user set ?',queryset,function(err,result){
+    		if(err){
+    			cosole.error(err);
+    			throw err;
+    		}
+    		console.log(queryset);
+    		socket.emit(data.name);
+    	});
+    	
+    	
     });
     
     socket.on('login', function (data) {
         console.log(data);
-        socket.emit('loginok',data);
+        
+        sqlconnection.query('select * from myDB.user',function(err, result){
+        	console.log(result);
+        	for(var shoot in result){
+        		
+        		if(result[shoot].Uemail == data.username){
+        			console.log("!!",md5(data.password));
+        			
+        			if(result[shoot].Upassword == md5(data.password)){
+        				 socket.emit("login"+data.username,result[shoot].Uname);
+        				 break;
+        			}
+        		}
+        	}
+        });
+        
+       
     });
     
     socket.on('logout', function () {
